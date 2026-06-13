1dacdfb70f393f882741ff8b5956715c65ef4348
ugwusomto <ugwujude42@gmail.com>
2026-06-13 17:57:01 +0200

    feat: add step to ensure default tenant exists in startup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,5 +19,13 @@ $BIN db sdp migrate up --all
 echo "Setting up assets and wallets for network..."
 $BIN db setup-for-network --all
 
+echo "Ensuring default tenant exists..."
+$BIN tenants ensure-default \
+  --default-tenant-owner-email "${DEFAULT_TENANT_OWNER_EMAIL:-owner@default.local}" \
+  --default-tenant-owner-first-name "${DEFAULT_TENANT_OWNER_FIRST_NAME:-Default}" \
+  --default-tenant-owner-last-name "${DEFAULT_TENANT_OWNER_LAST_NAME:-Owner}" \
+  --default-tenant-distribution-account-type "${DEFAULT_TENANT_DISTRIBUTION_ACCOUNT_TYPE:-DISTRIBUTION_ACCOUNT.STELLAR.ENV}" \
+  --sdp-ui-base-url "${SDP_UI_BASE_URL:-$BASE_URL}"
+
 echo "Starting server..."
 exec $BIN serve
